3ca31285b02454c54b64f2b37bfe2d8451e3c97c
Viní Marcili <vinipetter@gmail.com>
2021-05-10 14:57:57 -0300

    Base

---
+++ js/fontawesome.js
@@ -0,0 +1,7 @@
+function loadjs(file) {
+  var script = document.createElement("script")
+  script.type = "text/javascript"
+  script.src = file
+  document.body.appendChild(script)
+}
+loadjs('https://kit.fontawesome.com/91c8580ef2.js')
