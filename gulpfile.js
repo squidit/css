596c0260b225563c2a517f7ab9b0fdca9a9047b5
Viní Marcili <vinipetter@gmail.com>
2021-05-13 09:33:33 -0300

    Add fonts

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp')
+var sass = require('gulp-sass')    
+var uglify = require('gulp-uglify')
+var concat = require('gulp-concat')
+
+gulp.task('sass', function(done){
+  gulp.src('scss/squid.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('dist/css'))
+    done()
+})
+
+gulp.task('concat', function(done){
+  gulp.src('js/*.js')
+    .pipe(concat('squid.js'))
+    .pipe(gulp.dest('dist/js'))
+    done()
+})
+
+gulp.task('scripts', function(done){
+  gulp.src('js/*.js')
+    .pipe(concat('squid.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    done()
+})
+
+
+gulp.task('watch', function(done){
+  gulp.watch('scss/squid.scss', gulp.series('sass'))
+  gulp.watch('js/*.js', gulp.series('concat'))
+  done()
+})
