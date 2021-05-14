fec83f0b3ea41ca9d0d482ff30a3461c12e47ee4
Viní Marcili <vinipetter@gmail.com>
2021-05-14 16:21:01 -0300

    New folder style

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,21 @@ var uglify = require('gulp-uglify')
 var concat = require('gulp-concat')
 
 gulp.task('sass', function(done){
-  gulp.src('scss/squid.scss')
+  gulp.src('src/scss/squid.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
     done()
 })
 
 gulp.task('concat', function(done){
-  gulp.src('js/**/*.js')
+  gulp.src('src/js/**/*.js')
     .pipe(concat('squid.js'))
     .pipe(gulp.dest('dist/js'))
     done()
 })
 
 gulp.task('scripts', function(done){
-  gulp.src('js/**/*.js')
+  gulp.src('src/js/**/*.js')
     .pipe(concat('squid.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
@@ -26,12 +26,12 @@ gulp.task('scripts', function(done){
 })
 
 gulp.task('watch-js', function(done){
-  gulp.watch('js/**/*.js', gulp.series('concat'))
+  gulp.watch('src/js/**/*.js', gulp.series('concat'))
   done()
 })
 
 gulp.task('watch', function(done){
-  gulp.watch('scss/**/*.scss', gulp.series('sass'))
-  gulp.watch('js/**/*.js', gulp.series('concat'))
+  gulp.watch('src/scss/**/*.scss', gulp.series('sass'))
+  gulp.watch('src/js/**/*.js', gulp.series('concat'))
   done()
 })
