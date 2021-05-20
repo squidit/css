227f096a1e7517570a5d24089a577c260232a65e
Viní Marcili <vinipetter@gmail.com>
2021-05-20 09:36:39 -0300

    Add collapse component

--- dist/js/squid.js
+++ dist/js/squid.js
@@ -1,4 +1,37 @@
 // Global Variables or functions here
+function initAccordion (element) {
+  const collapses = [...element.children]
+  let currentTabIndex = -1
+
+  function setTab (index) {
+    if (index < 0) {
+      return
+    }
+    if (!collapses[index].classList.contains('disabled')) {
+      if (currentTabIndex > -1 && element.classList.contains('only-one')) {
+        if (index !== currentTabIndex) {
+          collapses[currentTabIndex].classList.remove('active')
+          collapses[index].classList.add('active')
+        } else {
+          collapses[index].classList.toggle('active')
+        }
+      } else {
+        collapses[index].classList.toggle('active')
+      }
+      currentTabIndex = index
+    }
+  }
+
+  defaultTabIndex = collapses.findIndex(x => [...x.classList].indexOf('active') > -1)
+  currentTabIndex = defaultTabIndex === -1 ? 0 : defaultTabIndex
+  setTab(currentTabIndex)
+  // eslint-disable-next-line
+  collapses.forEach((x, i) => x.onclick = event => setTab(i))
+}
+
+(function () {
+  [...document.querySelectorAll('.accordion')].forEach(x => initAccordion(x))
+})();
 
 
 
