2188b5bfea9ed3c538f40be5fc3789340d4a8a51
Viní Marcili <vinipetter@gmail.com>
2021-05-26 20:52:36 -0300

    Update Tag

---
+++ docs/doczrc.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'CSS - Components Squid Style',
+  description: 'Front-end framework style created by Squid',
+  menu: [
+    'Getting Started',
+    'Content',
+    'Layout',
+    'Forms',
+    'Components',
+    'Utilities',
+    'About'
+  ]
+}
