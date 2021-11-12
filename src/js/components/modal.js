95afe71a695d07499ec0365bae91b5c0a4df992c
Viní Marcili <vinipetter@gmail.com>
2021-11-12 10:26:09 -0300

    Add option to close modal when is clicked outsite

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -7,8 +7,25 @@ function createModal (event, trigger) {
   event.preventDefault()
   const modal = document.getElementById(trigger.dataset.modal)
   const backdrop = document.createElement('div')
+  const dataBackdrop = modal.getAttribute('data-backdrop')
   backdrop.setAttribute('id', 'modal-backdrop')
   backdrop.setAttribute('class', 'modal-backdrop show')
+  if (dataBackdrop !== 'static') {
+    modal.addEventListener('click', (event) => {
+      event.preventDefault()
+      const modalDialog = modal.firstElementChild
+      if (!modalDialog?.contains(event.target)) {
+        if (backdrop.parentNode) {
+          backdrop.parentNode.removeChild(backdrop)
+        }
+        modal.classList.remove('open')
+        setTimeout(() => {
+          modal.style.display = 'none'
+          body.classList.remove('block')
+        }, 500)
+      }
+    })
+  }
   document.body.appendChild(backdrop)
   const body = document.getElementsByTagName('body')[0]
   body.classList.add('block')
