0f60270b24ae33125c653b26cbb5a1df49485ea4
Viní Marcili <vinipetter@gmail.com>
2022-03-24 21:50:14 -0300

    Fix sass warnings

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp')
-var sass = require('gulp-sass')    
+const sass = require('gulp-sass')(require('sass'))  
 var uglify = require('gulp-uglify')
 var concat = require('gulp-concat')
 
