9da6a03bdba48ba7fe712d2b584cee47ca11db77
JonasPeres <jonasperes10@hotmail.com>
2023-12-05 17:31:44 -0300

    Add collapse on colors

--- src/js/squid.js
+++ src/js/squid.js
@@ -1,1 +1,25 @@
 // Global Variables or functions here
+
+const colors = [
+  'neutral',
+  'purple',
+  'pink',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'aqua',
+  'blue',
+  'lilac',
+  'instagram',
+  'youtube',
+  'pinterest',
+  'google',
+  'whatsapp',
+  'twitter',
+  'linkedin',
+  'facebook',
+  'x',
+  'tiktok',
+  'twitch'
+]
