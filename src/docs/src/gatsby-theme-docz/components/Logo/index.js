41d3bd4c8aaac43d74710216f0b4ab0e3f1fdf83
wan <wandersonsales.dev@gmail.com>
2024-07-11 17:15:48 -0300

    :sparkles: Change logo

--- src/docs/src/gatsby-theme-docz/components/Logo/index.js
+++ src/docs/src/gatsby-theme-docz/components/Logo/index.js
@@ -3,13 +3,17 @@ import { jsx, Flex } from 'theme-ui'
 import { Link, useConfig } from 'docz'
 import * as styles from './styles'
 
+const logoDark = 'https://hub-cdn.squidit.com.br/shared/prd/dist/f35992fdf9e5c1661a57.svg'
+const logoLight = 'https://hub-cdn.squidit.com.br/shared/prd/dist/50bf131db1c274381550.svg'
+
 export const Logo = () => {
   const config = useConfig()
   return (
     <Flex alignItems="center" className='justify-content-center' sx={styles.logo} data-testid="logo">
       <Link to="/" sx={styles.link}>
         <img
-          src='http://squidit.com.br/wp-content/themes/squid/assets/img/logo.svg'
+          width="100%"
+          src={document.body.parentElement.classList.contains('dark') ? logoDark : logoLight}
           alt='Squid Logo'
           title='Squid Logo'
           className='mr-3 img-fluid'
